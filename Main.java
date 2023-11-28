48fccf7890ecf83be248e226a88154316cb7e0c8
Yashas_J <yashasjraj@gmail.com>
2023-11-28 14:47:51 +0530

    jdbc single result set

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
+import java.sql.*;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Class.forName("org.postgresql.Driver");
+
+        Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/student","postgres","1442");
+
+        Statement st = con.createStatement();
+
+        ResultSet rs = st.executeQuery("select name,rollnum from student where classstd = 9");
+        rs.next();
+        System.out.println(rs.getString(1)+" "+rs.getInt(2));
+
+        rs.close();
+        st.close();
+        con.close();
     }
 }
